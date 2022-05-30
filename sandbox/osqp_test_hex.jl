6eeae7074bfc8cab97609ac4c17941aa932631d2
Zakariya Laouar <zala9003@colorado.edu>
2022-05-30 15:34:25 -0600

    mfmpc test

--- sandbox/osqp_test_hex.jl
+++ sandbox/osqp_test_hex.jl
@@ -6,6 +6,12 @@ using PlotlyJS
 using ControlSystems
 using PMPC_IMM.Hexacopter: LinearModel
 
+
+function blkdiag(A::AbstractMatrix, N::Int)
+    return cat(Iterators.repeated(A,N)..., dims=(1,2))
+end
+
+
 # Utility function
 speye(N) = spdiagm(ones(N))
 
@@ -16,12 +22,13 @@ A, B, C, D = lin_model.A, lin_model.B, lin_model.C, lin_model.D
 sys = ss(A, B, C, D)
 sysd = c2d(sys, Δt)
 Ad, Bd, H, D = sparse(sysd.A), sparse(sysd.B), sysd.C, sysd.D
+(nx, nu) = size(Bd)
 Bdfail = deepcopy(Bd)
 Bdfail = copyto!(Bdfail, zeros(nx,1))
 
+print("hello")
+display(Bd)
 
-
-(nx, nu) = size(Bd)
 
 # Constraints
 u0 = 15.5916
@@ -47,19 +54,20 @@ xr = [1, 2, -10, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 N = 10
 
 # Cast MPC problem to a QP: x = (x(0),x(1),...,x(N),u(0),...,u(N-1))
-M = 2 # number of scenarios
+M = 3 # number of scenarios
 
 # - quadratic objective
-P = blockdiag(kron(speye(N), Q), QN, kron(speye(N), R))
-xblk = P[1:132, 1:132]
-P = blockdiag(xblk, xblk, P[133:end, 133:end])
+P = blockdiag(blkdiag(blockdiag(kron(speye(N), Q), QN), M), kron(speye(N), R))
+
+#xblk = P[1:132, 1:132]
+#P = blockdiag(xblk, xblk, P[133:end, 133:end])
 # - linear objective
-q = [repeat(-Q * xr, N); -QN * xr; zeros(N*nu)]
-q = [repeat(q[1:132], M); q[133:end]]
+q = [repeat([repeat(-Q * xr, N); -QN * xr], M); zeros(N*nu)]
+#q = [repeat(q[1:132], M); q[133:end]]
 # - linear dynamics
-Ax = kron(speye(N + 1), -speye(nx)) + kron(spdiagm(-1 => ones(N)), Ad)
-Ablk = Ax[1:132,1:132]
-Ax = blockdiag(Ablk, Ablk)
+Ax = blkdiag(kron(speye(N + 1), -speye(nx)) + kron(spdiagm(-1 => ones(N)), Ad), M)
+#Ablk = Ax[1:132,1:132]
+#Ax = blockdiag(Ablk, Ablk)
 #Ax = kron(speye(N + 1), -speye(nx)) + kron(spdiagm(-1 => ones(N)), Ad)
 Bu = repeat(kron([spzeros(1, N); speye(N)], Bd), M)
 #Bu = kron([spzeros(1, N); speye(N)], Bd)
@@ -72,7 +80,7 @@ lineq = [repeat(xmin, M * (N + 1)); repeat(umin, N)]
 uineq = [repeat(xmax, M * (N + 1)); repeat(umax, N)]
 # - OSQP constraints
 A, l, u = [Aeq; Aineq], [leq; lineq], [ueq; uineq]
-
+Aold = deepcopy(A)
 # Create an OSQP model
 m = OSQP.Model()
 
@@ -81,11 +89,23 @@ OSQP.setup!(m; P=P, q=q, A=A, l=l, u=u, warm_start=true)
 
 # Simulate in closed loop
 nsim = 40;
+num_modes = 7
 xvec = Vector{Float64}[]
+uvec = Vector{Float64}[]
 push!(xvec, x0)
 tmp = Nothing
-
-@time for _ in 1 : nsim
+aval = A.nzval
+Bmat = ones(N, M)
+Bvec = Matrix{Float64}[]
+push!(Bvec, deepcopy(Bd))
+function genBvec!(Bvec::Vector{Matrix{Float64}})
+    for i in 1:num_modes-1
+        push!(Bvec, deepcopy(Bd))
+        last(Bvec)[:,i] .= 0.0
+    end
+end
+genBvec!(Bvec)
+@time for step in 1 : nsim
     # Solve
     global res = OSQP.solve!(m)
 
@@ -99,18 +119,29 @@ tmp = Nothing
     @info ctrl
     global x0 = Ad * x0 + Bd * ctrl
     push!(xvec, x0)
+    push!(uvec, ctrl)
 
-    # Update initial state
+    # Update initial state (for each scenario)
     for i in 1:M
         l[1 + (N+1)*nx*(i-1):nx *(1 + (N+1)*(i-1))] = -x0
         u[1 + (N+1)*nx*(i-1):nx *(1 + (N+1)*(i-1))] = -x0
     end
     
     # Update scenario B matrices 
-    Bu = [kron([spzeros(1, N); speye(N)], Bd); kron([spzeros(1, N); speye(N)], Bdfail)]
-    A[1:264, 265:end] = Bu
-    OSQP.update!(m; l=l, u=u, Ax=vec(A))
+    if step > 10
+        for i in size(Bmat)[2]
+            for i in size(Bmat)[1]
+                Bu[]
+            
+            end
+        end
+        Bu = [kron([spzeros(1, N); speye(N)], Bd); kron([spzeros(1, N); speye(N)], Bdfail)]
+        A[1:264, 265:end] = Bu
+        global avalnew = A.nzval
+    end
+    OSQP.update!(m; l=l, u=u, Ax=A.nzval)
 end
+
 
 
 
@@ -132,3 +163,14 @@ add_trace!(fig, scatter(x=tvec, y=-getindex.(hex_pos_true, 3),
 relayout!(fig, title_text="Hexacopter Position", yaxis_range=[-1,2],
             yaxis2_range=[-1,3], yaxis3_range=[-1,11])
 display(fig)
+
+# Plot control signals
+usignal = make_subplots(rows=3, cols=2, shared_xaxes=true, vertical_spacing=0.02, x_title="time(s)")
+add_trace!(usignal, scatter(x=tvec, y=getindex.(uvec, 1), name="rotor 1"), row=1, col=1)
+add_trace!(usignal, scatter(x=tvec, y=getindex.(uvec, 2)), row=1, col=2)
+add_trace!(usignal, scatter(x=tvec, y=getindex.(uvec, 3)), row=2, col=1)
+add_trace!(usignal, scatter(x=tvec, y=getindex.(uvec, 4)), row=2, col=2)
+add_trace!(usignal, scatter(x=tvec, y=getindex.(uvec, 5)), row=3, col=1)
+add_trace!(usignal, scatter(x=tvec, y=getindex.(uvec, 6)), row=3, col=2)
+
+display(usignal)
